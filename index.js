705a2339eaebe116c25ce50b60aaf8df02a08d96
meteorc <francois.artus@gmail.com>
2014-07-22 14:37:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // MODULES UTILISES
 // ========================================================
-var http 					= require("http");
-var mysql 					= require('mysql');
+var http 				= require("http");
+var mysql 				= require('mysql');
 var webRestServer			= require("./webRestServer");
 var calculerMoyenneMesures		= require('./calculerMoyenneMesures');
 var log					= require('./log');
@@ -14,7 +14,7 @@ GLOBAL.arduinoSerialPort		= "COM7";
 GLOBAL.arduinoSerialSpeed		= 9600;
 // Web/Rest server ------------------------------------------
 GLOBAL.myRestPort 			= process.env.OPENSHIFT_NODEJS_PORT 		|| 8080;		// Pour Openshift
-GLOBAL.myIpAddress 			= process.env.OPENSHIFT_NODEJS_IP 			|| 'localhost';	// Pour Openshift
+GLOBAL.myIpAddress 			= process.env.OPENSHIFT_NODEJS_IP 		|| 'localhost';	// Pour Openshift
 // Connexion mySql -------------------------------------------
 GLOBAL.mySqlHost 			= process.env.OPENSHIFT_MYSQL_DB_HOST 		|| "localhost";	// PourOpenshift
 GLOBAL.mySqlPort 			= process.env.OPENSHIFT_MYSQL_DB_PORT		|| 3306		// PourOpenshift
@@ -26,10 +26,10 @@ GLOBAL.correctionPressionNiveauMer	= 5;
 // Fhz des moyennes ------------------------------------------
 // Le serveur effectue le calcul
 // et stocage des mesures moyennes toutes les 10 mn (en millisecondes) :
-GLOBAL.fhzCalculMoyenneMesures	= 5 * 60 * 1000;
+GLOBAL.fhzCalculMoyenneMesures		= 5 * 60 * 1000;
 // Temps en ms ou bout duquel le contact avec la sonde est considéré comme perdu
 // (la sonde envoie normalement une mesure toutes les 10s)
-GLOBAL.tempsPerteContactSonde	= 15000;
+GLOBAL.tempsPerteContactSonde		= 15000;
 // Logging parameters ---------------------------------------
 GLOBAL.consoleLoggingLevel		= 4; 		// DETAIL
 GLOBAL.dbLoggingLevel			= 3;		// INFO
@@ -50,15 +50,15 @@ GLOBAL.sqlPool =  mysql.createPool({
   	password: mySqlPassword
 });
 // Gestion des télécommandes
-GLOBAL.commandNew = "00";
-GLOBAL.commandActive = "00";
-GLOBAL.commandBack = "00";
-GLOBAL.commandSent = true;
+GLOBAL.commandNew 	= "00";
+GLOBAL.commandActive 	= "00";
+GLOBAL.commandBack 	= "00";
+GLOBAL.commandSent 	= true;
 GLOBAL.commandSyncError = false;
 // Analyse des connexion
-GLOBAL.adresseGetData = {};
-GLOBAL.adresseSetData = {};
-GLOBAL.adresseGetLogs = {};
+GLOBAL.adresseGetData 	= {};
+GLOBAL.adresseSetData 	= {};
+GLOBAL.adresseGetLogs 	= {};
 
 // INITIALISATION
 // ========================================================
